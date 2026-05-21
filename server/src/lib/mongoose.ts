623906559ff2f3e146102cf59ac9aa0eb2bce829
sopuru-ani <sopuruani41@gmail.com>
2026-05-21 11:22:23 -0400

    bug fixes: expressx3

--- server/src/lib/mongoose.ts
+++ server/src/lib/mongoose.ts
@@ -2,17 +2,6 @@
  * Use the project root's mongoose so /db models and connectDB share one connection.
  * (A separate copy in server/node_modules caused buffering timeouts on login.)
  */
-import { createRequire } from "node:module";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
-const require = createRequire(import.meta.url);
-const rootDir = path.resolve(
-  path.dirname(fileURLToPath(import.meta.url)),
-  "../../..",
-);
-
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const mongoose = require(path.join(rootDir, "node_modules/mongoose")) as typeof import("mongoose");
+import mongoose from "mongoose";
 
 export default mongoose;
